73a2dfb5b55346d43e8248dc13718a0b7570cda1
Milton Khan <43887779+srmiltonkhan@users.noreply.github.com>
2021-09-25 23:32:17 +0600

    Add files via upload

---
+++ for_loop_structure.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, i,store;
+    printf("Enter Number of Elements: ");
+    scanf("%d",&n);
+    for(i=0; i<=n; ++i){
+        printf("Number %d =",i);
+        scanf("%d",&store);
+    }
+
+    return 0;
+}
