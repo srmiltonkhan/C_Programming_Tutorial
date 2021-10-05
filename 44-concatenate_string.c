1a3ea31cd10c65d1dc4f4d21e55fa3613d6133bc
Milton Khan <43887779+srmiltonkhan@users.noreply.github.com>
2021-10-05 09:48:29 +0600

    Add files via upload

---
+++ 44-concatenate_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	char str1[]="My name is";
+	char str2[]=" Milton";
+	
+	int i=0, j=0, len=0;
+	
+	while(str1[i]!='\0'){
+		i++;
+		len++;
+	}
+	while(str2[j] !='\0'){
+		str1[len+j]=str2[j];
+		j++;
+	}
+	
+	printf("%s ",str1);
+	
+	return 0;
+}
---
+++ 44.1-concatenate.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	char str1[]="He is a ";
+	char str2[]="student";
+	
+	strcat(str1,str2);
+	
+	printf("%s",str1);
+}
