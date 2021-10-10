8bbd53e21c4929f731f114ebbdda8377e73bd352
Milton Khan <43887779+srmiltonkhan@users.noreply.github.com>
2021-10-10 16:53:10 +0600

    Add files via upload

---
+++ Array/TraverseArray.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int num[100] = {1,2,3,4,5,6,7,8,9,10};
+	
+//	Traverse Array using for loop
+	int i;
+	for(i=0; i<10; i++){
+		printf("%d\n",num[i]);
+	}
+	
+	return 0;
+}
