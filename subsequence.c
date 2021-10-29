784538d7f390cac32d9ceebda48d82b5b8103472
Milton Khan <43887779+srmiltonkhan@users.noreply.github.com>
2021-10-29 12:28:10 +0600

    Update subsequence.c

--- subsequence.c
+++ subsequence.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
 #include <string.h>
+int check_subsequence (char [], char[]);
 
-int check_subsequence(char[], char[]);
+int main () {
+  int t;
+  char s1[1000], s2[1000];
 
-int main(){
-	int flag;
-	
-	char s1[50], s2[50];
-	
-	printf("Input The First String: \n");
-	gets(s1);
-	
-	printf("Input The Second String: \n");
-	gets(s2);
-	
-	if (strlen(s1) < strlen(s2))
-      flag = check_subsequence(s1, s2);
-   else
-      flag = check_subsequence(s2, s1);
+  printf("Input first string\n");
+  gets(s1);
 
-   (flag)?printf("YES\n"):printf("NO\n");
-	
-	return 0;
+  printf("Input second string\n");
+  gets(s2);
+
+  /** Passing smaller length string first */
+
+  if (strlen(s1) < strlen(s2))
+    t = check_subsequence(s1, s2);
+  else
+    t = check_subsequence(s2, s1);
+
+  if (t)
+    printf("YES\n");
+  else
+    printf("NO\n");
+
+  return 0;
+}
+
+int check_subsequence (char a[], char b[]) {
+  int c, d;
+
+  c = d = 0;
+
+  while (a[c] != '\0') {
+    while ((a[c] != b[d]) && b[d] != '\0') {
+      d++;
+    }
+    if (b[d] == '\0')
+      break;
+    d++;
+    c++;
+  }
+  if (a[c] == '\0')
+    return 1;
+  else
+    return 0;
 }
